475426523bc74136aedc2d5883b2ca47b7bea39f
hectwilliams <hectwilliams@gmail.com>
2019-04-20 08:47:33 -0700

    generation script -> 100 files with 1 files housing 100000 records (primary and secondary datasets)

--- database/bigMenuData.js
+++ database/bigMenuData.js
@@ -2,67 +2,68 @@ const faker = require('faker');
 const fs = require('file-system');
 const path = require('path');
 const csvWriter = require('csv-write-stream');
-const headers =  {
-  headers: [
-    'id',
-    'mealType',
-    'Item',
-    'Des',
-    'Price'
-  ]
+const headers = {
+  headers: ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'HappyHour']
 }
 const writer = csvWriter(headers);
 
+  var generateMenuObject_100000_helper = function() {
+    result = {};
+    var result = {'Breakfast':[], 'Lunch':[], 'Dinner':[], 'Brunch': [], 'HappyHour':[]};
+    var length = faker.random.number({min: 1, max: 3}) ;
 
-
-generateMenuObject = function(max, file ,callback) {
-  writer.pipe(fs.createWriteStream(file, {flags: 'a'}));
-    const types = ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'HappyHour'] ;
-    for (var r = 0; r < max; r++) {
-      for ( type of types ) {
-        for (var i = 0; i < faker.random.number({min: 2, max: 4}) ; i++) {
-          writer.write({
-            "id": i,
-            "mealType": JSON.stringify(faker.lorem.word()),
-            "Item": JSON.stringify( faker.lorem.paragraph()),
-            "Des": faker.lorem.paragraph(),
-            "Price": faker.commerce.price(),
-          })
-        }
+    for ( type of headers.headers ) {
+      for (var i = 0; i < length ; i++) {
+        result[type].push(JSON.stringify ({   itemName: faker.lorem.word(), itemDescription: faker.lorem.paragraph(), itemPrice: "$" + faker.commerce.price()} ));
       }
     }
-    callback(true)
-  }
+    return result;
+  };
 
-  recordGen = function( records,dir, callback) {
-    var directory = path.resolve('BIGDATA', 'Block' + String(dir));
-    var file = path.resolve('BIGDATA','Block' + String(dir), 'data' + '.csv' );
-      fs.mkdir(directory, { recursive: true } , (e) => {
-        generateMenuObject(records, file, (res) => {
+ var generateMenuObject_100000 = function(max, file ,callback) {
+  var result;
+  writer.pipe(fs.createWriteStream(file, {flags: 'a'}));
+    for (var r = 0; r < max; r++) {
+      result = generateMenuObject_100000_helper();
+      writer.write({
+        "id": r,
+        "Breakfast": result.Breakfast,
+        "Lunch": result.Lunch,
+        "Dinnner": result.Dinner,
+        "Brunch": result.Brunch,
+        "HappyHour": result.HappyHour
+      })
+    }
+    callback(true)
+  };
+
+  var generateBlock = function( records,dir, callback) {
+    var directory = path.resolve('TESTDATA', 'Block' + String(dir));
+    var file = path.resolve('TESTDATA','Block' + String(dir), 'data' + '.csv' );
+      fs.mkdir(directory, { recursive: true } , (e)=>{
+        generateMenuObject_100000(records, file, (res)=>{
           callback(res);
         });
       });
+    };
+
+
+  var build =  function( count = 0) {
+    const records = 100000;
+    const dirLimit = 100;
+
+    console.log('CURRENT COUNT', count);
+
+    if(count === dirLimit) {
+      process.exit();
     }
 
-
-    build =  function( count = 0, timeNow = Date.now()) {
-    const records = 100000; /* i.e. files limit */
-    const dirLimit = 100; /* i.e. directories limit*/
-
-      console.log('CURRENT COUNT', count);
-      if(count ===dirLimit) {
-        process.exit();
+    generateBlock(records,count, (bool )=>{
+      if (bool) {
+        build(++count );
       }
-
-      recordGen(records,count, (bool )=>{
-        if (bool) {
-          build(++count );
-        }
-      });
-   };
+    });
+  };
 
 
-exports.generator =  {
-  build
-}
-
+build();
