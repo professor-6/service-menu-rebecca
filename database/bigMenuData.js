d5799b6cccd41f02fc6df37d49bc73c654d59415
hectwilliams <hectwilliams@gmail.com>
2019-04-20 00:00:50 -0700

    Takes 40 seconds to generate 100000 records

--- database/bigMenuData.js
+++ database/bigMenuData.js
@@ -1,74 +1,68 @@
-var faker = require('faker');
-var fs = require('file-system');
-
-types =  { 'Breakfast': 3, 'Lunch': 3, 'Dinner': 3, 'Brunch': 3, 'HappyHour': 3 }
-
-var bigData = function() {
-  this.path = "BIGDATA/test_";
+const faker = require('faker');
+const fs = require('file-system');
+const path = require('path');
+const csvWriter = require('csv-write-stream');
+const headers =  {
+  headers: [
+    'id',
+    'mealType',
+    'Item',
+    'Des',
+    'Price'
+  ]
 }
-
-bigData.prototype.recordGen = function(id, records) {
-  var obj = {};
-  var file = `${this.path}${id}.json`;
+const writer = csvWriter(headers);
 
 
-  fs.open(file, 'wx', (err, fd) => {
-    console.log(fd, 'file desrc')
-    if (err)  console.log(fd, 'exists');;
 
-    for (var r=0; r<records; r ++) {
-      for (var key in types ) {
-        obj[key] = [];
-        for (var i = 0; i< types[key]; i++ ) {
-          obj[key].push({ itemName: faker.lorem.word(), itemDescription: faker.lorem.paragraph(), itemPrice: "$" + faker.commerce.price()});
+generateMenuObject = function(max, file ,callback) {
+  writer.pipe(fs.createWriteStream(file, {flags: 'a'}));
+    const types = ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'HappyHour'] ;
+    for (var r = 0; r < max; r++) {
+      for ( type of types ) {
+        for (var i = 0; i < faker.random.number({min: 2, max: 4}) ; i++) {
+          writer.write({
+            "id": i,
+            "mealType": JSON.stringify(faker.lorem.word()),
+            "Item": JSON.stringify( faker.lorem.paragraph()),
+            "Des": faker.lorem.paragraph(),
+            "Price": faker.commerce.price(),
+          })
         }
-        obj[key].push( "0xDEADBEEF"); //marker
       }
+    }
+    callback(true)
+  }
 
-
-      fs.appendFile(fd,  JSON.stringify(obj), 'UTF8', (err) => {
-
-        fs.close(fd, (err) => {
-          // if (err)  console.log('file desrc error');
+  recordGen = function( records,dir, callback) {
+    var directory = path.resolve('BIGDATA', 'Block' + String(dir));
+    var file = path.resolve('BIGDATA','Block' + String(dir), 'data' + '.csv' );
+      fs.mkdir(directory, { recursive: true } , (e) => {
+        generateMenuObject(records, file, (res) => {
+          callback(res);
         });
-        if (err) console.log('append file error');
       });
-
     }
 
 
-    });
+    build =  function( count = 0, timeNow = Date.now()) {
+    const records = 100000; /* i.e. files limit */
+    const dirLimit = 100; /* i.e. directories limit*/
+
+      console.log('CURRENT COUNT', count);
+      if(count ===dirLimit) {
+        process.exit();
+      }
+
+      recordGen(records,count, (bool )=>{
+        if (bool) {
+          build(++count );
+        }
+      });
+   };
 
 
+exports.generator =  {
+  build
 }
 
-  bigData.prototype.build_10_000 =  function() {
-    const nodes = 100;
-    const records = 1000;
-    var timeNow = Date.now();
-
-    for (var i=0; i < nodes ; i++) {
-      this.recordGen(i,records);
-      setTimeout(this.terminate.bind(this),10000,i);
-
-    }
-    console.log('time', Date.now() - timeNow)
-    timeNow=Date.now()
-
-  }
-
-  bigData.prototype.terminate = function(id) {
-    // console.log(read  /y);
-    const ext = '.json';
-    var file = this.path + id + ext;
-
-    // file processed remove from directory
-    console.log('removing file:', id);
-    fs.fs.unlink(file, (e)=>{
-      if(e) {
-        console.log('err')
-      }
-    });
-  }
-
-exports.generator = new bigData()
