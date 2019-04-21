0e89159558df0e9d1f173120c32055249a65bc21
hectwilliams <hectwilliams@gmail.com>
2019-04-21 00:14:56 -0700

    Generates 10 million in approximately 10 minutes. Major improvment from 50 minutes

--- database/bigMenuData.js
+++ database/bigMenuData.js
@@ -2,68 +2,53 @@ const faker = require('faker');
 const fs = require('file-system');
 const path = require('path');
 const csvWriter = require('csv-write-stream');
-const headers = {
-  headers: ['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'HappyHour']
-}
+const headers = { headers: ['0', '1', '2', '3', '4']};
+const  sim  = require('./pre-process-faker').fakerBuild();
 const writer = csvWriter(headers);
 
-  var generateMenuObject_100000_helper = function() {
-    result = {};
-    var result = {'Breakfast':[], 'Lunch':[], 'Dinner':[], 'Brunch': [], 'HappyHour':[]};
-    var length = faker.random.number({min: 1, max: 3}) ;
+var generateMenuObject_100000_helper = function() {
+  var result = {};
+  var idx =  faker.random.number({min: 0, max: 4}) ;
+  var mealType = (['Breakfast', 'Lunch', 'Dinner', 'Brunch', 'HappyHour'][idx] );
+  result['0'] = JSON.stringify ({meal: mealType, itemName: sim.word[idx], itemDescription: sim.paragraph[idx], itemPrice: "$" + sim.price[idx]});
+  result['1'] = JSON.stringify ({meal: mealType, itemName: sim.word[idx], itemDescription: sim.paragraph[idx], itemPrice: "$" + sim.price[idx]});
+  result['2'] = JSON.stringify ({meal: mealType, itemName: sim.word[idx], itemDescription: sim.paragraph[idx], itemPrice: "$" + sim.price[idx]});
+  result['3'] = JSON.stringify ({meal: mealType, itemName: sim.word[idx], itemDescription: sim.paragraph[idx], itemPrice: "$" + sim.price[idx]});
+  result['4'] = JSON.stringify ({meal: mealType,  itemName: sim.word[idx], itemDescription: sim.paragraph[idx], itemPrice: "$" + sim.price[idx]});
+  return result;
+};
 
-    for ( type of headers.headers ) {
-      for (var i = 0; i < length ; i++) {
-        result[type].push(JSON.stringify ({   itemName: faker.lorem.word(), itemDescription: faker.lorem.paragraph(), itemPrice: "$" + faker.commerce.price()} ));
-      }
-    }
-    return result;
-  };
+var generateMenuObject_100000 = function(max, file, callback) {
+writer.pipe(fs.createWriteStream(file, {flags: 'a'}));
+  for (var r = 0; r < max; r++) {
+    writer.write( generateMenuObject_100000_helper() );
+  }
+  callback(true)
+};
 
- var generateMenuObject_100000 = function(max, file ,callback) {
-  var result;
-  writer.pipe(fs.createWriteStream(file, {flags: 'a'}));
-    for (var r = 0; r < max; r++) {
-      result = generateMenuObject_100000_helper();
-      writer.write({
-        "id": r,
-        "Breakfast": result.Breakfast,
-        "Lunch": result.Lunch,
-        "Dinnner": result.Dinner,
-        "Brunch": result.Brunch,
-        "HappyHour": result.HappyHour
-      })
-    }
-    callback(true)
-  };
-
-  var generateBlock = function( records,dir, callback) {
-    var directory = path.resolve('TESTDATA', 'Block' + String(dir));
-    var file = path.resolve('TESTDATA','Block' + String(dir), 'data' + '.csv' );
-      fs.mkdir(directory, { recursive: true } , (e)=>{
-        generateMenuObject_100000(records, file, (res)=>{
-          callback(res);
-        });
-      });
-    };
-
-
-  var build =  function( count = 0) {
-    const records = 100000;
-    const dirLimit = 100;
-
-    console.log('CURRENT COUNT', count);
-
-    if(count === dirLimit) {
-      process.exit();
-    }
-
-    generateBlock(records,count, (bool )=>{
-      if (bool) {
-        build(++count );
-      }
+var generateBlock = function( records,fileNumber, callback) {
+  var file = path.resolve('TESTDATA' , 'data' + fileNumber + '.csv' );
+    generateMenuObject_100000(records, file, (res)=>{
+      callback(res);
     });
   };
 
+var build =  function( count=0 ) {
+  const records = 100000;
+  const dirLimit = 100;
+  console.log('LOG: current count', count);
 
-build();
+  if(count === dirLimit) {
+    console.log('LOG: -->  COMPLETE')
+    return ;
+  }
+
+  generateBlock(records,count, (bool )=>{
+    if (bool) {
+      setTimeout(build, 1500, ++count)
+    }
+  });
+};
+
+
+// build();
