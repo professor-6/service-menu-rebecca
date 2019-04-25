9efec803d04a7b9187968501f93f54a3b66fc335
hectwilliams <hectwilliams@gmail.com>
2019-04-25 16:47:46 -0700

    Refactoring Back-End Generation scripts now generates 500000 records per file resulting in 50 million database entries! Script execution time is approximately 9 minutes...YAY! Lastly, I added a new category_id header to my csv file.

--- database/bigMenuData.js
+++ database/bigMenuData.js
@@ -1,30 +1,39 @@
 const fs = require('file-system');
 const path = require('path');
 const sim  = require('./pre-process-faker').fakerBuild();
-var counter = 0;
+var counter = 1;
+var blockCounter =counter;
 
 var generateMenuObject_100000_helper = function() {
+  var stringBuffer = '';
   var result = {};
   var idx0 = Math.floor(Math.random()*5) ;
   var idx1 =  idx0 + 1;
   var idx2 =  idx1 + 1;
   var idx3 =  idx2 + 1;
   var idx4 =  idx3 + 1;
-  result['0'] = sim.word[idx0] +'-link-'  + sim.paragraph[idx0]+'-link-'+ sim.price[idx0]
-  result['1'] = sim.word[idx1] +'-link-'  + sim.paragraph[idx1]+'-link-'+ sim.price[idx1]
-  result['2'] =  sim.word[idx2] +'-link-'  + sim.paragraph[idx2]+'-link-'+ sim.price[idx2]
-  result['3'] = sim.word[idx3] +'-link-'  + sim.paragraph[idx3]+'-link-'+ sim.price[idx3]
-  result['4'] = sim.word[idx4] +'-link-'  + sim.paragraph[idx4]+'-link-'+ sim.price[idx4]
-  return counter++ + ',' + result[0] + ',' + result[1]  +', '+ result[2] + ','  + result[3] + ','  + result[4] + '\n';
+
+  result[0] = `"'{""itemName"":""${sim.word[idx0]}"", ""itemDescription"":""${sim.paragraph[idx0]}"", ""itemPrice"":""$${sim.price[idx0]}""}'"`;
+  result[1] = `"'{""itemName"":""${sim.word[idx1]}"", ""itemDescription"":""${sim.paragraph[idx1]}"", ""itemPrice"":""$${sim.price[idx1]}""}'"`;
+  result[2] = `"'{""itemName"":""${sim.word[idx2]}"", ""itemDescription"":""${sim.paragraph[idx2]}"", ""itemPrice"":""$${sim.price[idx2]}""}'"`;
+  result[3] = `"'{""itemName"":""${sim.word[idx3]}"", ""itemDescription"":""${sim.paragraph[idx3]}"", ""itemPrice"":""$${sim.price[idx3]}""}'"`;
+  result[4] = `"'{""itemName"":""${sim.word[idx4]}"", ""itemDescription"":""${sim.paragraph[idx4]}"", ""itemPrice"":""$${sim.price[idx4]}""}'"`;
+  for( var i= 0; i<5 ; i++) {
+    stringBuffer+= counter++ + ',' + (Math.floor ((blockCounter)) ||1 ) + ',' + result[Math.floor(Math.random()*5) ]  + ',' + result[Math.floor(Math.random()*5) ]  +','+ result[Math.floor(Math.random()*5) ] + ','  + result[Math.floor(Math.random()*5) ] + ','  + result[Math.floor(Math.random()*5) ] + '\n';
+  }
+  blockCounter++;
+  return stringBuffer;
 };
 
 var generateMenuObject_100000 = function(max, file, callback) {
+  var buffer= '';
+  fs.appendFileSync (file, "id,category_id,BREAKFAST, LUNCH, DINNER, BRUNCH, HAPPYHOUR\n", {option:{flags:'a+'}})
   for (var r = 0; r <= max; r++) {
     var result = generateMenuObject_100000_helper() ;
-    if(r==0) {
-      fs.appendFileSync (file, "id, BREAKFAST, LUNCH, DINNER, BRUNCH, HAPPYHOUR\n", {option:{flags:'a+'}})
-    }else {
-      fs.appendFileSync(file, result, {option:{flags:'a+'}})
+    buffer += result;
+    if( r%5000 ===0) {
+      fs.appendFileSync(file, buffer, {option:{flags:'a+'}})
+      buffer='';
     }
   }
   callback(true);
@@ -49,11 +58,11 @@ var build =  function ( count=0 ) {
 
   generateBlock(records,count, (bool )=>{
     if (bool) {
-      setTimeout(build, 2700, ++count)
+      build(++count);
     }
   });
 };
 
 
-// build();
+build();
 
