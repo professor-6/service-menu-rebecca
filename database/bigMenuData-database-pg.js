f8f8392c36901de25adfb0103342277f89c037ef
hectwilliams <hectwilliams@gmail.com>
2019-04-26 13:03:55 -0700

    Added 5x records to database generation script

--- database/bigMenuData-database-pg.js
+++ database/bigMenuData-database-pg.js
@@ -5,9 +5,9 @@ var Promise  = require('promise');
 var psqlCommands =  function (){
   this.pathCSVTest = path.resolve(__dirname, '..', 'TESTDATA')
 }
-
+//
 psqlCommands.prototype.entries_1_00_000 = function(fileNumber, callback) {
-  postgres.psqlClient.query(`COPY menu_selection (id,BREAKFAST,LUNCH,DINNER,BRUNCH,HAPPYHOUR) FROM '${this.pathCSVTest}/data${fileNumber}.csv'  WITH DELIMITER ',' CSV HEADER`)
+  postgres.psqlClient.query(`COPY menu_selection(id,BREAKFAST,LUNCH,DINNER,BRUNCH,HAPPYHOUR) FROM '${this.pathCSVTest}/data${fileNumber}.csv'  WITH DELIMITER ',' CSV HEADER`)
   .then( ()=>{
     if(fileNumber ===99 ){
       callback(true);
--- database/bigMenuData.js
+++ database/bigMenuData.js
@@ -2,36 +2,28 @@ const fs = require('file-system');
 const path = require('path');
 const sim  = require('./pre-process-faker').fakerBuild();
 var counter = 1;
-var blockCounter =counter;
+
+var nObjects = function(count) {
+var sample = '';
+  for (var i=0; i< count  ;  i++ ) {
+    sample +=`{""itemName"":""${sim.word[count ]}"", ""itemDescription"":""${sim.paragraph[count ]}"", ""itemPrice"":""$${sim.price[count]}""}` +  `${ i+1==count? '' : ','}`;
+  }
+  return `"'[${sample}]'"`
+};
 
 var generateMenuObject_100000_helper = function() {
   var stringBuffer = '';
-  var result = {};
-  var idx0 = Math.floor(Math.random()*5) ;
-  var idx1 =  idx0 + 1;
-  var idx2 =  idx1 + 1;
-  var idx3 =  idx2 + 1;
-  var idx4 =  idx3 + 1;
-
-  result[0] = `"'{""itemName"":""${sim.word[idx0]}"", ""itemDescription"":""${sim.paragraph[idx0]}"", ""itemPrice"":""$${sim.price[idx0]}""}'"`;
-  result[1] = `"'{""itemName"":""${sim.word[idx1]}"", ""itemDescription"":""${sim.paragraph[idx1]}"", ""itemPrice"":""$${sim.price[idx1]}""}'"`;
-  result[2] = `"'{""itemName"":""${sim.word[idx2]}"", ""itemDescription"":""${sim.paragraph[idx2]}"", ""itemPrice"":""$${sim.price[idx2]}""}'"`;
-  result[3] = `"'{""itemName"":""${sim.word[idx3]}"", ""itemDescription"":""${sim.paragraph[idx3]}"", ""itemPrice"":""$${sim.price[idx3]}""}'"`;
-  result[4] = `"'{""itemName"":""${sim.word[idx4]}"", ""itemDescription"":""${sim.paragraph[idx4]}"", ""itemPrice"":""$${sim.price[idx4]}""}'"`;
-  for( var i= 0; i<5 ; i++) {
-    stringBuffer+= counter++ + ',' + (Math.floor ((blockCounter)) ||1 ) + ',' + result[Math.floor(Math.random()*5) ]  + ',' + result[Math.floor(Math.random()*5) ]  +','+ result[Math.floor(Math.random()*5) ] + ','  + result[Math.floor(Math.random()*5) ] + ','  + result[Math.floor(Math.random()*5) ] + '\n';
-  }
-  blockCounter++;
+    stringBuffer+= counter++ + ',' +  nObjects( Math.ceil(Math.random()*5) ) + ',' +  nObjects( Math.ceil(Math.random()*5) ) +','+  nObjects( Math.ceil(Math.random()*5) )+ ','  +  nObjects( Math.ceil(Math.random()*5) ) + ','  +  nObjects( Math.ceil(Math.random()*5) )+ '\n';
   return stringBuffer;
 };
 
 var generateMenuObject_100000 = function(max, file, callback) {
   var buffer= '';
-  fs.appendFileSync (file, "id,category_id,BREAKFAST, LUNCH, DINNER, BRUNCH, HAPPYHOUR\n", {option:{flags:'a+'}})
+  fs.appendFileSync (file, "id,BREAKFAST, LUNCH, DINNER, BRUNCH, HAPPYHOUR\n", {option:{flags:'a+'}})
   for (var r = 0; r <= max; r++) {
     var result = generateMenuObject_100000_helper() ;
     buffer += result;
-    if( r%5000 ===0) {
+    if( r%25000 ===0) {
       fs.appendFileSync(file, buffer, {option:{flags:'a+'}})
       buffer='';
     }
@@ -40,7 +32,7 @@ var generateMenuObject_100000 = function(max, file, callback) {
 };
 
 var generateBlock = function ( records,fileNumber, callback) {
-  var file = path.resolve('test2' , 'data' + fileNumber + '.csv' );
+  var file = path.resolve('TESTDATA' , 'data' + fileNumber + '.csv' );
   generateMenuObject_100000(records, file, (res)=>{
     callback(res);
   });
@@ -64,5 +56,5 @@ var build =  function ( count=0 ) {
 };
 
 
-build();
+// build();
 
